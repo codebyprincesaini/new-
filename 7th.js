eee60a266d9dd77b7c1c8b3d51399f9c5a1d2bf5
codebyprincesaini <sainiprinc324@gmail.com>
2024-04-10 22:30:21 +0530

    6,7th

--- 7th.js
+++ 7th.js
@@ -81,16 +81,113 @@
 
 // class5
 
-let nums =[2, 3, 4, 5, 3, 4, 7, 8, 1, 2];
+// let nums =[2, 3, 4, 5, 3, 4, 7, 8, 1, 2];
 
-let result = nums.reduce( ( max, el) =>{
-    if(el > max){
-        return el;
-    }else{
-        return max;
-    }
-} );
+// let result = nums.reduce( ( max, el) =>{
+//     if(el > max){
+//         return el;
+//     }else{
+//         return max;
+//     }
+// } );
 
-console.log(result)
+// console.log(result)
+
+// class 6 defult function 
+
+// function sum (a,b =3){
+//     return a+b;
+// }
+
+// sum(5);
+// console.log(sum(5))
+
+// class7 8 9  spread with object literals
+
+// let data ={
+//     email: "sainiprinc324@gmail.com",
+//     password: "abcd",
+// };
+
+// const dataCopy ={...data ,id:123 , country:"india"};
+
+
+// let arr =[1, 2, 3, 4, 5, ];
+// let obj1 ={...arr};
+
+
+// class10 Rest
+
+
+// function sum(...args){
+//     for(let i=0; i<args.length; i++){
+//         console.log("you gave us: ",args[i]);
+//     }
+// }
+
+// function min(){
+//     console.log(arguments);
+//     console.log(arguments.length);
+//     arguments.push(1);
+// }
+
+
+// function sum(...args){
+//     return args.reduce((sum,el) => sum +el);
+// }
+
+// function min (msg, ...args){
+//     console.log(msg);
+//     return args.reduce((min,el) =>{
+//         if (min>el){
+//             return el;
+//         }else{
+//             return min;
+//         }
+//     });
+// }
+
+// class11 Destructuring
+
+// let names =["tony", "bruce", "steve","peter"];
+// let[winner, runnerup] = names;
+// console.log(winner, runnerup,);
+
+// class12 destructuring in object 
+
+const student ={
+    name:"karan",
+    class:9,
+    age:14,
+    subjects: ["hindi", "english", "math", "science", "social studis"],
+    username: "karan123",
+    password: 1234,
+};
+
+const {username: user, password: pass} =student;
+
+console.log(user);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
